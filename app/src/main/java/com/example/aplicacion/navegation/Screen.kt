1c15c35ac819f63a9b3b7203aa5fde58282086ab
BMORA <ben.morag@duocuc.cl>
2025-11-20 13:17:23 -0300

    Agregando Screen.kt en paquete navegation

--- app/src/main/java/com/example/aplicacion/navegation/Screen.kt
+++ app/src/main/java/com/example/aplicacion/navegation/Screen.kt
@@ -1,2 +1,12 @@
-package com.example.aplicacion.navegation
+package com.example.simplemanager.navigation
 
+/**
+ * Define las rutas de navegación de la aplicación.
+ */
+sealed class Screen(val route: String) {
+    object Login : Screen("login")
+    object Register : Screen("register")
+    object Home : Screen("home")
+    object Profile : Screen("profile")
+    object Location : Screen("location")
+}
