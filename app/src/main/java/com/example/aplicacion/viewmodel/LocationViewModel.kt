a18e5ecc3e0ba923cfbac2dd75588de44af3f435
BMORA <ben.morag@duocuc.cl>
2025-11-20 14:13:19 -0300

    Agregando LocationViewModel

--- app/src/main/java/com/example/aplicacion/viewmodel/LocationViewModel.kt
+++ app/src/main/java/com/example/aplicacion/viewmodel/LocationViewModel.kt
@@ -1,2 +1,36 @@
-package com.example.aplicacion.viewmodel
+package com.example.simplemanager.viewmodel
 
+import android.location.Location
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+/**
+ * Estado que contiene los datos de geolocalización.
+ */
+data class LocationState(
+    val latitude: Double? = null,
+    val longitude: Double? = null,
+    val status: String = "Esperando ubicación..."
+)
+
+/**
+ * ViewModel para la pantalla de Ubicación.
+ */
+class LocationViewModel : ViewModel() {
+    var locationState by mutableStateOf(LocationState())
+        private set
+
+    fun updateLocation(location: Location) {
+        locationState = LocationState(
+            latitude = location.latitude,
+            longitude = location.longitude,
+            status = "Ubicación obtenida con éxito"
+        )
+    }
+
+    fun updateStatus(status: String) {
+        locationState = locationState.copy(status = status)
+    }
+}
